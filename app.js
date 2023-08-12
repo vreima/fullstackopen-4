a69fe628ff09ca30ca6f957937f1dd92db92bdfd
Ville <ville.reima@gmail.com>
2023-08-12 22:19:30 +0300

    Ex 4.1

---
+++ app.js
@@ -0,0 +1,13 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+app.use(blogRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 // const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+
 const config = require("./utils/config");
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-mongoose.connect(config.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+const app = require("./app");
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
